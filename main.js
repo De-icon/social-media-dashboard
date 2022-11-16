4516adb97873c519c070e65a972643e3f2e8cc7e
isaac john <isaacjohn2028@gmail.com>
2022-11-16 21:31:43 +0100

    new changes

---
+++ main.js
@@ -0,0 +1,9 @@
+// function toggle() {
+//     alert("hey u ")
+// }
+const darkMode = document.getElementById("darkMode")
+
+darkMode.addEventListener('click', () => {
+   darkMode.classList.toggles("active")
+}
+   
