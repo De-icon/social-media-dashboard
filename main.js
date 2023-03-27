671b81d9bbe04424d6e06766a1e9c4abb5e4dce9
isaac john <isaacjohn2028@gmail.com>
2023-03-28 00:07:56 +0100

    added the switch

--- main.js
+++ main.js
@@ -2,8 +2,19 @@
 //     alert("hey u ")
 // }
 const darkMode = document.getElementById("darkMode")
+const social = document.getElementById("social")
+const titel = document.getElementById("titel")
+const item = document.getElementsById("item")
+const overView = document.getElementById("overView")
+const header = document.getElementById("header")
 
-darkMode.addEventListener('click', () => {
-   darkMode.classList.toggles("active")
+darkMode.onclick = () => {
+   // social.classList.toggle('active')
+   // titel.classList.toggle('active')
+   // item.classList.toggle('active')
+   // overView.classList.toggle('active')
+   // darkMode.classList.toggle('active')
+   // header.classList.toggle('active')
+   console.log("word");
+
 }
-   
